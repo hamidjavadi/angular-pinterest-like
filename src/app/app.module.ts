2705be7353db7c121cf0b8efca808435b8aa4517
Hamid Javadi <javadi.business@gmail.com>
2022-04-16 19:29:23 +0430

    - Base components created - Fontawesome added - Router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IndexComponent } from './pages/index/index.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostComponent } from './components/post/post.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailsComponent } from './pages/post-details/post-details.component';
+import { routes } from 'src/app/router/routes';
+import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    IndexComponent,
+    PostComponent,
+    NavbarComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(routes),
     StoreModule.forRoot({}, {})
   ],
   providers: [],
